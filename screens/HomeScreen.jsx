c87ef25b635ad93e785d18bf8bde92970d512dd5
kaydan95 <kyori0515@gmail.com>
2022-11-08 17:55:00 +0900

    + 검색 섹션 추가

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { UserIcon, ChevronDownIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
+import { UserIcon, ChevronDownIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon, AdjustmentsHorizontalIcon } from "react-native-heroicons/outline";
 
 
 const HomeScreen = () => {
@@ -29,10 +29,19 @@ const HomeScreen = () => {
                     <Text className='text-xs text-gray-400 font-bold'>Deliever now!</Text>
                     <Text className='text-xl font-bold'>
                         Current Location
-                        <ChevronDownIcon size={22} color="#00CCBB"/>
+                        <ChevronDownIcon size={22} color="#698474"/>
                     </Text>
                 </View>
-                <UserIcon size={33} color="#00CCBB"/>
+                <UserIcon size={33} color="#698474"/>
+            </View>
+
+            {/* search section */}
+            <View className='flex-row items-center space-x-2 px-3 pb-3'>
+                <View className='flex-row flex-1 bg-[#F3F3F3] space-x-2 p-2 rounded-lg'>
+                    <MagnifyingGlassIcon size={22} color="#698474" />
+                    <TextInput className='text-[#698474]' placeholder='Keep our planet' keyboardType='default'/>
+                </View>
+                <AdjustmentsHorizontalIcon size={22} color="#698474"/>
             </View>
         </SafeAreaView>
     )
