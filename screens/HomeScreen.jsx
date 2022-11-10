83c7129c0b051bf7b117ce790824a25bfc210756
kaydan95 <kyori0515@gmail.com>
2022-11-10 14:42:37 +0900

    indent 정리

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -6,51 +6,54 @@ import TopBrandSection from '../components/TopBrandSection';
 
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown : false,
-        });
-    }, []);
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      {/* header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-8 w-8 p-4 rounded-full "
+        />
 
+        <View className="w-fit flex-col flex-1 justify-center">
+          <Text className="text-xs text-gray-400 font-bold">Deliever now!</Text>
+          <Text className="text-xl font-bold">
+            Current Location
+            <ChevronDownIcon size={22} color="#464E2E" />
+          </Text>
+        </View>
+        <UserIcon size={33} color="#464E2E" />
+      </View>
 
-    return (
-        <SafeAreaView className='bg-white pt-5'>
-            {/* header */}
-            <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-                <Image source={{
-                    uri : 'https://links.papareact.com/wru',
-                }}
-                className='h-8 w-8 p-4 rounded-full '/>
-                
-                
-                <View className='w-fit flex-col flex-1 justify-center'>
-                    <Text className='text-xs text-gray-400 font-bold'>Deliever now!</Text>
-                    <Text className='text-xl font-bold'>
-                        Current Location
-                        <ChevronDownIcon size={22} color="#464E2E"/>
-                    </Text>
-                </View>
-                <UserIcon size={33} color="#464E2E"/>
-            </View>
+      {/* search section */}
+      <View className="flex-row items-center space-x-2 px-3 pb-3">
+        <View className="flex-row flex-1 bg-[#F3F3F3] space-x-2 p-2 rounded-lg">
+          <MagnifyingGlassIcon size={22} color="#464E2E" />
+          <TextInput
+            className="text-[#464E2E]"
+            placeholder="Keep our planet"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsHorizontalIcon size={22} color="#464E2E" />
+      </View>
 
-            {/* search section */}
-            <View className='flex-row items-center space-x-2 px-3 pb-3'>
-                <View className='flex-row flex-1 bg-[#F3F3F3] space-x-2 p-2 rounded-lg'>
-                    <MagnifyingGlassIcon size={22} color="#464E2E" />
-                    <TextInput className='text-[#464E2E]' placeholder='Keep our planet' keyboardType='default'/>
-                </View>
-                <AdjustmentsHorizontalIcon size={22} color="#464E2E"/>
-            </View>
+      {/* body */}
+      <ScrollView>
+        <TopBrandSection className="absolute" />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-            {/* body */}
-            <ScrollView>
-                <TopBrandSection className='absolute'/>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
