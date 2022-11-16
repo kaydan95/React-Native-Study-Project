10919a1202a08022a09b3033c34dcc4a6a308520
kaydan95 <kyori0515@gmail.com>
2022-11-16 16:49:58 +0900

    :heavy_plus_sign: 로그인화면 추가

---
+++ screens/LogInScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+
+const LogInScreen = () => {
+  return (
+    <View>
+      <Text>LogInScreen</Text>
+      <Button title="LOGIN" />
+    </View>
+  );
+};
+
+export default LogInScreen;
